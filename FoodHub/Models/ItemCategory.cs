1a937b55fd8c220884812f27b7d26d50824d0151
Sebbern <Sebastianeckbo@gmail.com>
2024-10-21 16:56:02 +0200

    add ItemCategory model and a one-to-many with Item.cs

---
+++ FoodHub/Models/ItemCategory.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace FoodHub.Models {
+    public class ItemCategory
+    {
+        public int ItemCategoryId { get; set; }
+        
+        [StringLength(200)]
+        public string Name { get; set; } = string.Empty;
+
+        //Navigation property for the related items
+        public virtual List<Item> Items { get; set; } = new List<Item>();
+    }
+}
