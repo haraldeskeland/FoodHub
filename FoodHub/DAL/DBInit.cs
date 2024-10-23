d117e0b9e3deadcdd09f5f3fc5a4c79680b7122a
Sebbern <Sebastianeckbo@gmail.com>
2024-10-23 14:12:09 +0200

    Added new db seeding

--- FoodHub/DAL/DBInit.cs
+++ FoodHub/DAL/DBInit.cs
@@ -15,10 +15,11 @@ public static class DBInit
         {
             var itemcategories = new List<ItemCategory>
             {
+                //Categories taken from the national norwegian food groups within "Nøkkelhullforskriften", excluding the smaller categories
                 new ItemCategory { ItemCategoryId = 1, Name = "Grønnsaker, frukt, bær og nøtter"},      
                 new ItemCategory { ItemCategoryId = 2, Name = "Mel, gryn og ris"},        
                 new ItemCategory { ItemCategoryId = 3, Name = "Grøt, brød og pasta"},
-                new ItemCategory { ItemCategoryId = 4, Name = "Melk og syrnede melkeprodukter"},
+                new ItemCategory { ItemCategoryId = 4, Name = "Animalske produkter, som egg, melk og syrnede melkeprodukter"},
                 new ItemCategory { ItemCategoryId = 5, Name = "Vegetabilske alternativer til melkeprodukter"},
                 new ItemCategory { ItemCategoryId = 6, Name = "Ost og vegetabilske alternativer"},
                 new ItemCategory { ItemCategoryId = 7, Name = "Matfett (margariner) og oljer"},
@@ -35,7 +36,9 @@ public static class DBInit
         //Temp variables for seeded database entries
         var greensCategory = context.ItemCategories.First(c => c.ItemCategoryId == 1);
         var pastaCategory = context.ItemCategories.First(c => c.ItemCategoryId == 3);
+        var animalCategory = context.ItemCategories.First(c => c.ItemCategoryId == 4);
         var meatCategory = context.ItemCategories.First(c => c.ItemCategoryId == 9);
+        var fastfoodCategory = context.ItemCategories.First(c => c.ItemCategoryId == 11);
 
         if (!context.Items.Any())
         {
@@ -44,7 +47,8 @@ public static class DBInit
                 new Item
                 {
                     Name = "Pizza Grandiosa Original",
-                    ImageUrl = "/images/pizza.jpg",
+                    ProducerName = "Orkla",
+                    ImageUrl = "/images/grandiosa-original.png",
                     Description = "Grandiosa Original has a filling of tomato sauce, pizza meat and paprika. It is topped with Jarlsberg®, which gives the pizza a taste of its own. The original Grandiosa was launched in 1980 and since then has been by far Norway's most eaten pizza. ",
                     Energy = 218M,
                     Carbohydrate = 26M,
@@ -60,104 +64,111 @@ public static class DBInit
                 },
                 new Item
                 {
-                    Name = "Fried Chicken Leg",
-                    ImageUrl = "/images/chickenleg.jpg",
-                    Description = "Crispy and succulent chicken leg that is deep-fried to perfection, often served as a popular fast food item.",
-                    Energy = 300M,
-                    Carbohydrate = 15M,
-                    TotalFat = 20M,
-                    SaturatedFat = 6M,
-                    UnsaturedFat = 10M,
-                    Sugar = 0M,
-                    DietaryFiber = 0M,
-                    Protein = 25M,
-                    Salt = 1.5M,
-                    ItemCategoryId = meatCategory.ItemCategoryId,
-                    ItemCategory = meatCategory
-                },
-                new Item
-                {
-                    Name = "French Fries",
-                    ImageUrl = "/images/frenchfries.jpg",
-                    Description = "Crispy, golden-brown potato slices seasoned with salt and often served as a popular side dish or snack.",
-                    Energy = 365M,
-                    Carbohydrate = 63M,
-                    TotalFat = 17M,
-                    SaturatedFat = 2M,
-                    UnsaturedFat = 15M,
-                    Sugar = 0M,
-                    DietaryFiber = 4M,
-                    Protein = 4M,
-                    Salt = 0.6M,
-                    ItemCategoryId = meatCategory.ItemCategoryId,
-                    ItemCategory = meatCategory
-                },
-                new Item
-                {
-                    Name = "Grilled Ribs",
-                    ImageUrl = "/images/ribs.jpg",
-                    Description = "Tender and flavorful ribs grilled to perfection, usually served with barbecue sauce.",
-                    Energy = 400M,
-                    Carbohydrate = 10M,
-                    TotalFat = 30M,
-                    SaturatedFat = 12M,
-                    UnsaturedFat = 15M,
-                    Sugar = 5M,
-                    DietaryFiber = 0M,
-                    Protein = 25M,
-                    Salt = 2M,
-                    ItemCategoryId = meatCategory.ItemCategoryId,
-                    ItemCategory = meatCategory
-                },
-                new Item
-                {
-                    Name = "Tacos",
-                    ImageUrl = "/images/tacos.jpg",
-                    Description = "Tortillas filled with various ingredients such as seasoned meat, vegetables, and salsa, folded into a delicious handheld meal.",
-                    Energy = 200M,
-                    Carbohydrate = 30M,
-                    TotalFat = 8M,
-                    SaturatedFat = 3M,
-                    UnsaturedFat = 5M,
-                    Sugar = 1M,
-                    DietaryFiber = 3M,
-                    Protein = 10M,
+                    Name = "Kylling Tikka 480g, Fersk & Ferdig",
+                    ProducerName = "Unil",
+                    ImageUrl = "/images/fersk-ferdig-tikka.jpg",
+                    Description = "Ferdig Kylling Tikka inspirert av det indiske kjøkken. Kyllingbiter i deilig masala-saus med basmatiris. Server sammen med naan-brød og chutney. Rask og enkel middag. Varmes i micro.",
+                    Energy = 138M,
+                    Carbohydrate = 13M,
+                    TotalFat = 5.2M,
+                    SaturatedFat = 1.5M,
+                    UnsaturedFat = 3.7M,
+                    Sugar = 2.6M,
+                    DietaryFiber = 1.3M,
+                    Protein = 9.3M,
                     Salt = 0.5M,
-                    ItemCategoryId = meatCategory.ItemCategoryId,
-                    ItemCategory = meatCategory
+                    ItemCategoryId = fastfoodCategory.ItemCategoryId,
+                    ItemCategory = fastfoodCategory
                 },
                 new Item
                 {
-                    Name = "Fish and Chips",
-                    ImageUrl = "/images/fishandchips.jpg",
-                    Description = "Classic British dish featuring battered and deep-fried fish served with thick-cut fried potatoes.",
-                    Energy = 550M,
+                    Name = "Opphøgde Potteter - Originalen",
+                    ProducerName = "HOFF",
+                    ImageUrl = "/images/Opphogde-Originalen.png",
+                    Description = "HOFF Opphøgde Potteter Originalen har blitt laget på fabrikken i Gjøvik i flere generasjoner. Med sin sprøe overflate og myke innside har den blitt en av nordmenns favoritter! Produktet stekes ferdig på bare 9 minutter i ovnen.",
+                    Energy = 198M,
+                    Carbohydrate = 34M,
+                    TotalFat = 6M,
+                    SaturatedFat = 0.7M,
+                    UnsaturedFat = 5.3M,
+                    Sugar = 1.3M,
+                    DietaryFiber = 0M,
+                    Protein = 2M,
+                    Salt = 0M,
+                    ItemCategoryId = greensCategory.ItemCategoryId,
+                    ItemCategory = greensCategory
+                },
+                new Item
+                {
+                    Name = "Egg Frittgående, 12stk",
+                    ProducerName = "Prior",
+                    ImageUrl = "/images/egg-prior.jpg",
+                    Description = "Egg fra norske bønder hvor hønene har tilgang på elementer som gir økt trivsel.",
+                    Energy = 149M,
+                    Carbohydrate = 0.3M,
+                    TotalFat = 11M,
+                    SaturatedFat = 3M,
+                    UnsaturedFat = 8M,
+                    Sugar = 0.3M,
+                    DietaryFiber = 0M,
+                    Protein = 13M,
+                    Salt = 0.4M,
+                    ItemCategoryId = animalCategory.ItemCategoryId,
+                    ItemCategory = animalCategory
+                },
+                new Item
+                {
+                    Name = "YT Proteinmelk Kakao, 1L",
+                    ProducerName = "Tine",
+                    ImageUrl = "/images/yt-proteinmelk-kakao-1l.jpg",
+                    Description = "YT® Proteinmelk Kakao er sunn kakaomelk med 68 % mer protein enn annen sjokolademelk. I tillegg så er den fettfri, laktoseredusert og uten tilsatt sukker, med naturlig sukker fra melka",
+                    Energy = 45M,
+                    Carbohydrate = 4.7M,
+                    TotalFat = 0.3M,
+                    SaturatedFat = 0.2M,
+                    UnsaturedFat = 0.1M,
+                    Sugar = 4.5M,
+                    DietaryFiber = 0M,
+                    Protein = 6M,
+                    Salt = 0.1M,
+                    ItemCategoryId = animalCategory.ItemCategoryId,
+                    ItemCategory = animalCategory
+                },
+                new Item
+                {
+                    Name = "Olivenolje, 500ml",
+                    ProducerName ="Eldorado",
+                    ImageUrl = "/images/olivenolje-eldorado.jpg",
+                    Description = "En klassisk, spansk olivenolje med en rund og mild smak. Passer godt til pasta, steking og baking.",
+                    Energy = 900M,
                     Carbohydrate = 60M,
-                    TotalFat = 30M,
-                    SaturatedFat = 7M,
-                    UnsaturedFat = 20M,
+                    TotalFat = 100M,
+                    SaturatedFat = 14M,
+                    UnsaturedFat = 86M,
                     Sugar = 0M,
-                    DietaryFiber = 3M,
-                    Protein = 20M,
-                    Salt = 1M,
+                    DietaryFiber = 0M,
+                    Protein = 0M,
+                    Salt = 0M,
                     ItemCategoryId = meatCategory.ItemCategoryId,
                     ItemCategory = meatCategory
                 },
 
                 new Item
                 {
-                    Name = "Coke",
-                    ImageUrl = "/images/coke.jpg",
-                    Description = "Popular carbonated soft drink known for its sweet and refreshing taste.",
-                    Energy = 140M,
-                    Carbohydrate = 39M,
+                    Name = "Coca-Cola Zero Sugar",
+                    ProducerName = "Coca-Cola Europacific Partners Norge",
+                    ImageUrl = "/images/coca-cola-zero-sugar.webp",
+                    Description = "Coca-Cola® Zero Sugar ble lansert i 2005, under navnet Coca-Cola® zero. Coca-Cola® Zero Sugar ligger så tett opp til originalen som mulig, og er en del av strategien for å bidra til å redusere folks sukkerinntak.",
+                    Energy = 0.3M,
+                    Carbohydrate = 0M,
                     TotalFat = 0M,
                     SaturatedFat = 0M,
                     UnsaturedFat = 0M,
-                    Sugar = 39M,
+                    Sugar = 0M,
                     DietaryFiber = 0M,
                     Protein = 0M,
-                    Salt = 0.02M,
+                    Salt = 0M,
+                    //Natrium = 0.02M,
                     ItemCategoryId = meatCategory.ItemCategoryId,
                     ItemCategory = meatCategory
                 },
---
+++ FoodHub/Migrations/20241023120619_AddProducerNameToItems.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProducerNameToItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProducerName",
+                table: "Items",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProducerName",
+                table: "Items");
+        }
+    }
+}
