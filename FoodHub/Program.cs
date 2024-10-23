9f09bd1e9a8a7b885012b1eaff96fe2e3864bd37
Sebbern <Sebastianeckbo@gmail.com>
2024-10-23 19:03:35 +0200

    Remove unused code

--- FoodHub/Program.cs
+++ FoodHub/Program.cs
@@ -20,50 +20,13 @@ builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<Ite
 
 builder.Services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
 
-
-// builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
-// {
-//     // Password settings
-//     options.Password.RequireDigit = true;
-//     options.Password.RequiredLength = 8;
-//     options.Password.RequireNonAlphanumeric = true;
-//     options.Password.RequireUppercase = true;
-//     options.Password.RequireLowercase = true;
-//     options.Password.RequiredUniqueChars = 6;
-
-//     // Lockout settings
-//     options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(60);
-//     options.Lockout.MaxFailedAccessAttempts = 5;
-//     options.Lockout.AllowedForNewUsers = true;
-
-//     // User settings
-//     options.User.RequireUniqueEmail = true;
-
-//     // Sign-in settings
-//     options.SignIn.RequireConfirmedAccount = false; // Set to true if you want email confirmation
-// })
-// .AddEntityFrameworkStores<ItemDbContext>()
-// .AddDefaultTokenProviders();
-
-// builder.Services.ConfigureApplicationCookie(options =>
-// {
-//     options.LoginPath = "/Identity/Account/Login"; // Ensure this path is valid
-// });
-
-
 builder.Services.AddScoped<IItemRepository, ItemRepository>();
 
-builder.Services.AddRazorPages(); // order of adding services does not matter
+builder.Services.AddRazorPages();
 builder.Services.AddSession();
-// builder.Services.AddSession(options =>
-// {
-//     options.Cookie.Name = ".AdventureWorks.Session";
-//     options.IdleTimeout = TimeSpan.FromSeconds(1800); // 30 minutes
-//     options.Cookie.IsEssential = true;
-// });
 
 var loggerConfiguration = new LoggerConfiguration()
-    .MinimumLevel.Information() // levels: Trace< Information < Warning < Erorr < Fatal
+    .MinimumLevel.Information()
     .WriteTo.File($"Logs/app_{DateTime.Now:yyyyMMdd_HHmmss}.log");
 
 loggerConfiguration.Filter.ByExcluding(e => e.Properties.TryGetValue("SourceContext", out var value) &&
