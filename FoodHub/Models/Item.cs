43097379823b7fbe0935fac13c602c3eb9432258
Sebbern <Sebastianeckbo@gmail.com>
2024-10-31 18:56:43 +0100

    Change ImageUrl into ImagePath and add more input validation

--- FoodHub/Models/Item.cs
+++ FoodHub/Models/Item.cs
@@ -3,17 +3,19 @@ using System.ComponentModel.DataAnnotations;
 namespace FoodHub.Models {
     public class Item
     {
-        // Primary key for the Item entity
+        // Primary key
         [Key]
         public int ItemId { get; set; }
 
         // Name of the item with validation rules
-        [RegularExpression(@"[0-9a-zA-Zæøå. \-]{2,100}", ErrorMessage = "The name must be numbers or letters and between 2 and 20 characters")]
+        [Required(ErrorMessage = "Item name is required")]
+        [RegularExpression(@"[0-9a-zA-Zæøå. \-]{1,100}", ErrorMessage = "The item's name must be between 1 and 100 characters")]
         [Display(Name = "Item Name")]
         public string Name { get; set; } = string.Empty;
         
         // Name of the producer with validation rules
-        [RegularExpression(@"[0-9a-zA-Zæøå. \-]{2,100}", ErrorMessage = "The name must be numbers or letters and between 2 and 20 characters")]
+        [Required(ErrorMessage = "Producer name is required")]
+        [RegularExpression(@"[0-9a-zA-Zæøå. \-]{1,100}", ErrorMessage = "The producer's name must be between 1 and 100 characters")]
         [Display(Name = "Producer Name")]
         public string ProducerName { get; set; } = string.Empty;
 
@@ -21,47 +23,48 @@ namespace FoodHub.Models {
         [StringLength(600)]
         public string? Description { get; set; }
 
-        // URL for the image representing the item
-        public string? ImageUrl { get; set; }
+        // Path for the image representing the item
+        public string? ImagePath { get; set; }
 
-        // Nutritional Information
-        // Energy in kcal with validation to ensure non-negative values
-        [Range(0, double.MaxValue, ErrorMessage = "Energy must be a non-negative value")]
+        // Nutritional Information with input validation to ensure positive values
+        [Required(ErrorMessage = "Energy in kcal is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Energy must be a positive value")]
         public decimal Energy { get; set; }
 
-        // Carbohydrate in grams with validation to ensure non-negative values
-        [Range(0, double.MaxValue, ErrorMessage = "Carbohydrate must be a non-negative value")]
+        [Required(ErrorMessage = "Carbohydrates in gram is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Carbohydrate must be a positive value")]
         public decimal Carbohydrate { get; set; }
 
-        // Total fat in grams with validation to ensure non-negative values
-        [Range(0, double.MaxValue, ErrorMessage = "Total fat must be a non-negative value")]
+        [Required(ErrorMessage = "Total fat in gram is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Total fat must be a positive value")]
         public decimal TotalFat { get; set; }
 
-        // Saturated fat in grams with validation to ensure non-negative values
-        [Range(0, double.MaxValue, ErrorMessage = "Saturated fat must be a non-negative value")]
+        [Required(ErrorMessage = "Saturated fat in gram is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Saturated fat must be a positive value")]
         public decimal SaturatedFat { get; set; }
 
-        // Unsaturated fat in grams with validation to ensure non-negative values
-        [Range(0, double.MaxValue, ErrorMessage = "Unsaturated fat must be a non-negative value")]
+        [Required(ErrorMessage = "Unsatured fat in gram is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Unsaturated fat must be a positive value")]
         public decimal UnsaturedFat { get; set; }
 
-        // Sugar in grams with validation to ensure non-negative values
-        [Range(0, double.MaxValue, ErrorMessage = "Sugar must be a non-negative value")]
+        [Required(ErrorMessage = "Sugar in gram is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Sugar must be a positive value")]
         public decimal Sugar { get; set; }
 
-        // Dietary fiber in grams with validation to ensure non-negative values
-        [Range(0, double.MaxValue, ErrorMessage = "Dietary fiber must be a non-negative value")]
-        public decimal? DietaryFiber { get; set; }
+        [Required(ErrorMessage = "Dietary fiber in gram is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Dietary fiber must be a positive value")]
+        public decimal DietaryFiber { get; set; }
 
-        // Protein in grams with validation to ensure non-negative values
-        [Range(0, double.MaxValue, ErrorMessage = "Protein must be a non-negative value")]
+        [Required(ErrorMessage = "Protein in gram is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Protein must be a positive value")]
         public decimal Protein { get; set; }
 
-        // Salt in grams with validation to ensure non-negative values
-        [Range(0, double.MaxValue, ErrorMessage = "Salt must be a non-negative value")]
+        [Required(ErrorMessage = "Salt in gram is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Salt must be a positive value")]
         public decimal Salt { get; set; }
 
         // Foreign key for ItemCategory
+        [Required]
         public int ItemCategoryId { get; set; }
 
         // Navigation property for the relevant ItemCategory
