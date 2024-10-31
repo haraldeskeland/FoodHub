58b7b0db1b606ab52e47a8543c0c0bede247606c
Sebbern <Sebastianeckbo@gmail.com>
2024-10-31 18:56:00 +0100

    Remove ImageUrl from the allergen model

--- FoodHub/Models/Allergen.cs
+++ FoodHub/Models/Allergen.cs
@@ -2,17 +2,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace FoodHub.Models {
     public class Allergen {
-        // Primary key for the Allergen entity
+        // Primary key
         [Key]
         public int AllergenId { get; set; }
 
-        // Name of the allergen with validation rules
-        [RegularExpression(@"[0-9a-zA-Zæøå. \-]{2,100}", ErrorMessage = "The name must be numbers or letters and between 2 and 20 characters")]
-        [Display(Name = "Item Name")]
+        // Allergen name, no further input validation as allergens are seeded via dbinit
+        [Required]
         public string Name { get; set; } = string.Empty;
-
-        // URL for the image representing the allergen
-        public string? ImageUrl { get; set; } = string.Empty;
 
         // Many-to-many relationship with Item
         public virtual ICollection<ItemAllergen> ItemAllergen { get; set; } = new List<ItemAllergen>();
