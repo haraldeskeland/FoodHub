82299b2aab8f2838326437a4ea2ae95f549c1e67
Sebbern <Sebastianeckbo@gmail.com>
2024-11-25 15:24:57 +0100

    Pushing React test

---
+++ FoodHubReact/foodhub/src/App.js
@@ -0,0 +1,12 @@
+import FrontPage from './home/FrontPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FrontPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ FoodHubReact/foodhub/src/home/FrontPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FrontPage = () => {
+    return (
+        <div className="text-center">
+            <h1 className="display-4">test</h1>
+        </div>
+    );
+};
+
+export default FrontPage
