1ccb4a7a33e83c8320b50a4703d16096f0b66020
haraldeskeland <harald_mathias@hotmail.com>
2024-11-28 17:24:15 +0100

    Fix gradient text variable

--- FoodHubReact/foodhub/src/home/HomePage.tsx
+++ FoodHubReact/foodhub/src/home/HomePage.tsx
@@ -11,14 +11,16 @@ const HomePage: React.FC = () => {
     navigate(`/items?search=${encodeURIComponent(searchQuery)}`);
   };
 
+  
+
   return (
     <div className="main-container max-w-full lg:max-w-[2000px] mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
       {/* Section containing the title and search bar */}
-      <div className="kontainer w-full max-w-[1400px] flex justify-center items-center min-h-[40vh]">
+      <div className="kontainer w-full max-w-[1400px] flex justify-center items-center min-h-[40vh] mt-12">
         <div className="text-center w-full mx-auto">
           {/* Main title of the page with animations and gradient text */}
           <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-6xl font-medium tracking-tight gradient-text-large mb-4" data-aos="fade-up">
-            Find out what you´re <span className="gradient-text underline pr-2 font-extrabold dark:drop-shadow-[0_0_15px_rgba(59,130,246,0.5)]"><i>actually</i></span> eating
+            Find out what you´re <span className="gradient-text pr-2 font-extrabold dark:drop-shadow-[0_0_15px_rgba(59,130,246,0.5)]"><i>actually</i></span> eating
           </h1>        
           {/* Supporting description text with fade-up animation */}
           <p className="text-md font-light text-gray-500 mt-6 dark:text-slate-400" data-aos="fade-up" data-aos-delay="100">
--- FoodHubReact/foodhub/tailwind.config.js
+++ FoodHubReact/foodhub/tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function({ addUtilities, theme }) {
+      const gradientTextUtilities = {
+        '.gradient-text': {
+          'background-image': 'linear-gradient(to right, #3490dc, #6574cd, #9561e2)',
+          '-webkit-background-clip': 'text',
+          'background-clip': 'text',
+          '-webkit-text-fill-color': 'transparent',
+          'color': '#30a8bc', // Fallback color
+          'display': 'inline-block',
+        },
+      }
+      addUtilities(gradientTextUtilities)
+    }
+  ],
 }
 
