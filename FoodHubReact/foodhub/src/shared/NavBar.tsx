41232ca36ae8df7b790fa16a3826f87302a7d436
haraldeskeland <harald_mathias@hotmail.com>
2024-11-29 14:00:40 +0100

    Fix search bar in navbar dark mode

--- FoodHubReact/foodhub/src/shared/NavBar.tsx
+++ FoodHubReact/foodhub/src/shared/NavBar.tsx
@@ -50,8 +50,8 @@ const NavMenu: React.FC = () => {
               {!isHomePage && (
                 <form onSubmit={handleSearch} className="flex-grow max-w-md">
                   <div className="flex w-full">
-                    <div className="flex items-center justify-center rounded-l-full border border-r-0 border-gray-300 dark:bg-[#1d1d1f] dark:border-[#303030d5] bg-white px-4">
-                      <svg viewBox="0 0 20 20" aria-hidden="true" className="w-5 h-5 fill-gray-500">
+                    <div className="flex items-center justify-center rounded-l-full border-t border-b border-l border-gray-300 bg-white dark:!bg-[#1d1d1f] dark:!border-[#303030d5] px-4">
+                  <svg viewBox="0 0 20 20" aria-hidden="true" className="w-5 h-5 fill-gray-500">
                         <path d="M16.72 17.78a.75.75 0 1 0 1.06-1.06l-1.06 1.06ZM9 14.5A5.5 5.5 0 0 1 3.5 9H2a7 7 0 0 0 7 7v-1.5ZM3.5 9A5.5 5.5 0 0 1 9 3.5V2a7 7 0 0 0-7 7h1.5ZM9 3.5A5.5 5.5 0 0 1 14.5 9H16a7 7 0 0 0-7-7v1.5Zm3.89 10.45 3.83 3.83 1.06-1.06-3.83-3.83-1.06 1.06ZM14.5 9a5.48 5.48 0 0 1-1.61 3.89l1.06 1.06A6.98 6.98 0 0 0 16 9h-1.5Zm-1.61 3.89A5.48 5.48 0 0 1 9 14.5V16a6.98 6.98 0 0 0 4.95-2.05l-1.06-1.06Z"></path>
                       </svg>
                     </div>
@@ -60,7 +60,7 @@ const NavMenu: React.FC = () => {
                       value={searchQuery}
                       onChange={(e) => setSearchQuery(e.target.value)}
                       placeholder="Search for your favourite food..."
-                      className="flex-grow py-2 px-2 bg-white dark:bg-[#1d1d1f] dark:border-[#303030d5] text-base outline-none border-y border-gray-300"
+                      className="flex-grow py-2 px-2 bg-white dark:!bg-[#1d1d1f] dark:border-[#303030d5] text-base outline-none border-y border-gray-300"
                     />
                     <button type="submit" className="bg-gray-800 px-4 py-2 rounded-r-full text-white hover:bg-blue-800 transition-colors">
                       Search
