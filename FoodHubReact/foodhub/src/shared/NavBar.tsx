6eef2860716163522525b7c767408c8b853ca64d
haraldeskeland <harald_mathias@hotmail.com>
2024-11-30 17:09:14 +0100

    Remove mobile toggle button in navbar

--- FoodHubReact/foodhub/src/shared/NavBar.tsx
+++ FoodHubReact/foodhub/src/shared/NavBar.tsx
@@ -73,11 +73,7 @@ const NavMenu: React.FC = () => {
                 >
                   <img src="https://localhost:7268/images/light-dark.png" alt="Toggle theme" className="w-full h-full filter dark:invert" />
                 </button>
-                <button className="sm:hidden" type="button" id="navbar-toggle">
-                  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-                  </svg>
-                </button>
+                
               </div>
             </div>
           </div>
