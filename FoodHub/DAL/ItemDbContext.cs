4aeb88ee7ed5930a3fcd04646177837a9cc5bf36
Sebbern <Sebastianeckbo@gmail.com>
2024-12-01 13:14:44 +0100

    Fix nullable warnings

--- FoodHub/DAL/ItemDbContext.cs
+++ FoodHub/DAL/ItemDbContext.cs
@@ -15,10 +15,10 @@ namespace FoodHub.DAL
         }
 
         // DbSet properties representing tables in the database
-        public DbSet<Item> Items { get; set; }
-        public DbSet<ItemCategory> ItemCategories { get; set; }
-        public DbSet<Allergen> Allergens { get; set; }
-        public DbSet<ItemAllergen> ItemAllergens { get; set; }
+        public DbSet<Item>? Items { get; set; }
+        public DbSet<ItemCategory>? ItemCategories { get; set; }
+        public DbSet<Allergen>? Allergens { get; set; }
+        public DbSet<ItemAllergen>? ItemAllergens { get; set; }
 
         // Configuring the DbContext to use lazy loading proxies
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
