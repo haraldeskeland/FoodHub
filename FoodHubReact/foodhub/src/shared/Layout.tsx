588b618468afdc3e9fadf7d58b836a49711feab4
haraldeskeland <harald_mathias@hotmail.com>
2024-12-02 00:00:54 +0100

    Remove footer from app2

--- FoodHubReact/foodhub/src/shared/Layout.tsx
+++ FoodHubReact/foodhub/src/shared/Layout.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Footer from './Footer'; // Path based on your shared folder structure
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -9,7 +8,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow">{children}</main>
-      <Footer />
     </div>
   );
 };
