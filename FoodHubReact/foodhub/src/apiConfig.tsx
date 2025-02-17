1d2aaa79814a823da5b2f35f20c4b72eb84b7608
haraldeskeland <harald_mathias@hotmail.com>
2025-02-17 17:34:51 +0100

    Deploy React app

--- FoodHubReact/foodhub/src/apiConfig.tsx
+++
@@ -1,5 +0,0 @@
-// Get the API URL from the environment variables or use an empty string as a fallback
-const API_URL = process.env.REACT_APP_API_URL || '';
-
-// Export the API URL as the default export
-export default API_URL;
